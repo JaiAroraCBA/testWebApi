3d8e155f9060dc8d45b87bff100c9f5bd367375c
aleks1 <aleksey.shkirko@gmail.com>
2015-10-21 18:29:55 +0500

    1

---
+++ testWebApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testWebApi.Startup))]
+namespace testWebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
